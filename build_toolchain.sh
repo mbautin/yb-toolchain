77a39244353a2bf2666bffcdbe100ee06d5e418b
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-04-25 03:40:14 +0000

    Fix clone depth

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 yum install -y flex texinfo help2man gperf bison
 ctng_tag=crosstool-ng-1.24.0
-git clone https://github.com/crosstool-ng/crosstool-ng --branch "$ctng_tag" -depth 1 "$ctng_tag"
+git clone https://github.com/crosstool-ng/crosstool-ng --branch "$ctng_tag" --depth 1 "$ctng_tag"
 cd "$ctng_tag"
 ./bootstrap
 ctng_prefix=/usr/share/$ctng_tag
